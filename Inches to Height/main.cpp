c9dd8c21664656716238f75460ecf8e8712d8aa7
DC Cruz <dccoder@icloud.com>
2017-07-20 15:42:59 -0700

    Convert Height App
    Convert Height App

---
+++ Inches to Height/main.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    cout << "DC Cruz July 8th 2017"<<endl <<endl;
+    int height,height_feet,height_inches; // height inches we do not need to ocnvert we just need to hold and add to
+    cout<<"Please enter your height in feet and inches: ";
+    cin>>height_feet>>height_inches;
+    height=height_feet*12+height_inches; // simple equation taking feet 8 12 and adding tht height inches already given.
+    cout<<"Your height of "<<height_feet<<"'"<<height_inches<<"\" equals "<<height<<" inches."<<endl;
+
+}
